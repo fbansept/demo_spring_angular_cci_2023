cdd8883d35698a928df671fe7cb095480a551d9e
fbansept <bansept.franck@gmail.com>
2023-02-01 11:01:39 +0100

    11h

--- src/main/java/com/example/demo/controllers/ArticleController.java
+++ src/main/java/com/example/demo/controllers/ArticleController.java
@@ -3,12 +3,15 @@ package com.example.demo.controllers;
 import com.example.demo.dao.ArticleDao;
 import com.example.demo.models.Article;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@CrossOrigin
 public class ArticleController {
 
     @Autowired
@@ -18,6 +21,21 @@ public class ArticleController {
     public List<Article> getListeArticle() {
 
         return articleDao.findAll();
+
+    }
+
+    @PostMapping("/article")
+    public ResponseEntity<Article> ajoutArticle(@RequestBody Article article) {
+
+        Optional<Article> articleBdd = articleDao.findByTitre(article.getTitre());
+
+        //Un article porte déjà ce nom
+        if(articleBdd.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        articleDao.save(article);
+        return new ResponseEntity<>(article,HttpStatus.CREATED);
 
     }
 }
